5aabccfbbf901ade75ef01170a8fbf354672f6e6
RickyLi11228 <124058517+RickyLi11228@users.noreply.github.com>
2023-10-19 10:12:50 -0400

    dom

--- index.js
+++ index.js
@@ -1,13 +1,17 @@
 const DOMSelectors = {
     form: document.querySelector("#form"),
     firstName: document.querySelector(".first-name"),
-    h2s:document.querySelectorAll("h2"),
+    h2s: document.querySelectorAll("h2"),
 };
 
 DOMSelectors.form.addEventListener("submit", function(event){
    event.preventDefault();
    console.log(DOMSelectors.firstName.value);
    DOMSelectors.h2s.forEach(
-    (el)=> el.textContent = (DOMSelectors.firstName.value)
-   );
-});
+    (el)=> el.textContent = (DOMSelectors.firstName.value),
+    document.querySelector(".gallery").insertAdjacentHTML
+    ("afterbegin",
+    `<div class="card"><h2 class="card-header">
+    ${DOMSelectors.firstname.value}</h2></div>`
+    )
+   )});
