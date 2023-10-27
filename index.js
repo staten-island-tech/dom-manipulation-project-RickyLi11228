d5f465e244d09a6f100b1d6e067a4981c4b0474d
RickyLi11228 <124058517+RickyLi11228@users.noreply.github.com>
2023-10-27 10:13:54 -0400

    so close

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const DOMSelectors = {
     input: document.querySelector(`#input`),
     input2: document.querySelector(`#input2`),
     remove: document.querySelector(`#remove`),
+    input3: document.querySelector(`#input3`)
 };
 
 DOMSelectors.form.addEventListener("submit", function(event){
@@ -17,19 +18,24 @@ DOMSelectors.form.addEventListener("submit", function(event){
 const card = DOMSelectors.button.addEventListener("click", function(){
     let input= DOMSelectors.input.value;
     let input2= DOMSelectors.input2.value;
+    let input3 = DOMSelectors.input3.value;
     DOMSelectors.form.insertAdjacentHTML("beforeend",
     `<div class="gallery">
     <div class="card">
         <h2 class="card-header" id="column">${input2}</h2>
+        <img src="${input3}" alt="" class="img">
         <h3 class="card-info" id="column">${input}</h3>
-        <img src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAOEAAADhCAMAAAAJbSJIAAAAOVBMVEX/3IezeRz/4Yy7hS341H+ucQyxdhjIlkC/ijL/4Ir20X3XqVW0ex3/3om3fiSvcw3/5pDiuGSsbgS5rKJeAAABJElEQVR4nO3cS1LDMBBAweB8IJCEkPsfNiuzwiVVtJgZ1H0CP5c3kkfa7QAAAAAAAAAAAAAAAAAAoOk6KPr5m66HQekTl9txyPcSXdCy7N/GKAynUKHCeAoVKoynUKHCeNMWnrpVLXzvdo4uaPm78HJfOqUP3CpM/+31U1ifwvoU1qewPoX1Kaxv2sL7uSH9r99fGyvgr5afMokv7mIc86/tVy/uRJ0U5qFQYX4KFeanUGF+ChXmN21ha/j5Ub3wcvhsyD/Bvtraiap/CmE17V6bwkIU1qewPoX1KaxPYX3TFnbPCOd/FRtr/I9u6RNHZ/XLnkbopzCcQoUK4ylUqDCeQoUK401QeHv887u+Rq+ki356AAAAAAAAAAAAAAAAAACAcU9O3yl3aqTFWQAAAABJRU5ErkJggg==" alt=""
-        class="img">
     </div>
     <button type="submit" id="remove">Remove</button>`);
     DOMSelectors.input.value = "";
     DOMSelectors.input2.value = "";
+    DOMSelectors.input3.value = "";
 });
-DOMSelectors.delete.addEventListener("submit", function(){
-    delete card
-});
-card
+function remove(){
+let remove = document.querySelectorAll("remove");
+remove.forEach((remove)=> remove.addEventListener('click', function(event){
+    event.target.parentElement.remove();
+})
+);
+}
+remove();
